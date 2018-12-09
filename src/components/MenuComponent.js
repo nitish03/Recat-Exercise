12338ddea69124fc030be7b02d933f635fce1fc6
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-12-09 15:23:43 -0500

    add Media from reactstrap

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+
+class Menu extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<div classname="container">
+			  <div className="row">
+				  <Media list>
+					</Media>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Menu;
